d8bde5c6f4498f9834df9c377041e04ea4c5536c
skriaga2014 <9aec645aA>
2022-03-06 20:48:55 +0300

    inheritance

--- products/views.py
+++ products/views.py
@@ -4,25 +4,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Geekshop'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
-
-def test_context(request):
     context = {
-        'title': 'Geekshop - Текстовый контекст',
-        'header': 'Welcome!',
-        'username': 'Иван Иванов',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
-            {'name': 'Синяя куртка The North Face', 'price': 23725},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 6090},
-        ],
-        'is_promotion': True,
-
+        'title': 'Geekshop - Продукты'
     }
+    return render(request, 'products/products.html', context)
 
-    return render(request, 'products/test-context.html', context)
+
 
