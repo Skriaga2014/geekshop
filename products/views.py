ca4637065b4ae778b3c2db4175c79bfd128bd2f8
skriaga2014 <9aec645aA>
2022-03-07 00:54:55 +0300

    remove_testcontext

--- products/views.py
+++ products/views.py
@@ -27,19 +27,3 @@ def products(request):
     return render(request, 'products/products.html', context)
 
 
-def test_context(request):
-    context = {
-        'title': 'Geekshop - Текстовый контекст',
-        'header': 'Welcome!',
-        'username': 'Иван Иванов',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
-            {'name': 'Синяя куртка The North Face', 'price': 23725},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 6090},
-        ],
-        'is_promotion': True,
-
-    }
-
-    return render(request, 'products/test-context.html', context)
-
